4a852f7bd95cdf1e5533853301865fe60c30eefb
Miguel Angel Ramos <return.infox@gmail.com>
2024-05-08 20:32:08 -0400

    formulario de registro compltado

--- memberships/views.py
+++ memberships/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from .forms import CustomUserCreationForm
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'memberships/register.html', {'form': form })
